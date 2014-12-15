448db6b56e5342163771cd75921c5899ae6517a7
Nick Smelik <kerzok11@gmail.com>
2014-12-16 00:57:06 +0300

    initial commit

---
+++ source/TopPuzzle.ru/Controllers/PuzzleController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TopPuzzle.ru.Controllers
+{
+    public class PuzzleController : Controller
+    {
+        // GET: Puzzle
+        public ActionResult Editor()
+        {
+            return View();
+        }
+    }
+}
---
+++ source/TopPuzzle.ru/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TopPuzzle.ru.Startup))]
+namespace TopPuzzle.ru
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
