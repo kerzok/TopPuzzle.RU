d26077173fa51597238137ad82710a28c697a483
Sachbanoid <sachbanoid@gmail.com>
2014-12-19 15:05:14 +0400

    Todo-хи

---
+++ source/PictureLoader/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PictureLoader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //TODO: "../../../../Pictures"
+            //Download 100 pictures
+            //1.jpg, 2.jpg...
+        }
+    }
+}
