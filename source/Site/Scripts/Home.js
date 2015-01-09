60d2bc8e09bba99aac7832fad968e35b85f2d521
Egor Aleynikov <dr.alan666@gmail.com>
2015-01-09 17:14:56 +0300

    scores on main page

---
+++ source/Site/Scripts/Home.js
@@ -0,0 +1,10 @@
+﻿$(document).ready(function() {
+    $("#scores").load("/scores", null, null);
+
+    $(".complexity").click(function () {
+        var complexity = $(this).attr("complexity");
+        $("#scores").empty();
+        $("#scores").load("/scores", { "complexity": complexity }, null);
+    });
+});
+
