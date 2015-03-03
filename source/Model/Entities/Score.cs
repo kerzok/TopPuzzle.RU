b60935c2e37c0e313bbcc700a1a9756e17fea10d
Alexander Rakovsky <str1t3r@gmail.com>
2015-03-04 01:36:46 +0300

    выбор картинки по id в базе

--- source/Model/Entities/Score.cs
+++ source/Model/Entities/Score.cs
@@ -3,7 +3,7 @@
 namespace Toppuzzle.Model.Entities {
     public class Score {
         public int UserId { get; set; }
-        public string PictureId { get; set; }
+        public int PictureId { get; set; }
         public int Complexity { get; set; }
         public int Time { get; set; }
         public DateTime Date { get; set; }
